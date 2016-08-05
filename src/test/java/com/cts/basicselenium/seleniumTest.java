bbbbfecc8f3f661b0e1cfd1b2c182a31582bc22b
Administrator <Administrator@ip-192-168-56-1.us-west-2.compute.internal>
2016-08-05 05:03:00 +0000

    commit # 1

---
+++ src/test/java/com/cts/basicselenium/seleniumTest.java
@@ -0,0 +1,55 @@
+package com.cts.basicselenium;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class seleniumTest {
+  private String baseUrl;
+  private WebDriver driver;
+  private ScreenshotHelper screenshotHelper;
+  
+  @Before
+  public void openBrowser() {
+   // baseUrl = System.getProperty("webdriver.base.url");
+    driver = new ChromeDriver();
+    driver.get("http://www.google.com");
+    screenshotHelper = new ScreenshotHelper();
+  }
+  
+  @After
+  public void saveScreenshotAndCloseBrowser() throws IOException {
+    screenshotHelper.saveScreenshot("screenshot.png");
+    driver.quit();
+  }
+  
+  @Test
+  public void pageTitleCompare() throws IOException {
+    assertEquals("The page title should equal Google at the start of the test.", "Google", driver.getTitle());
+    //WebElement searchField = driver.findElement(By.name("q"));
+    
+  }
+  
+  private class ScreenshotHelper {
+  
+    public void saveScreenshot(String screenshotFileName) throws IOException {
+      File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+      FileUtils.copyFile(screenshot, new File(screenshotFileName));
+    }
+  }
+}
