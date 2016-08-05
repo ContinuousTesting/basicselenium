3e37525a70f98af33dc133c3817996c7d1c245a5
Administrator <Administrator@ip-192-168-56-1.us-west-2.compute.internal>
2016-08-05 08:23:59 +0000

    commit

--- src/test/java/com/cts/basicselenium/seleniumTest.java
+++ src/test/java/com/cts/basicselenium/seleniumTest.java
@@ -27,7 +27,7 @@ public class seleniumTest {
   public void openBrowser() {
 	  
 	    
-	System.out.println("Called openBrowser-");
+	System.out.println("Called openBrowser-123");
 	System.setProperty("webdriver.chrome.driver", "C:\\Apps\\SeleniumWebdrivers\\chromedriver.exe");
 	Map<String, Object> chromeOptions = new HashMap<String, Object>();
 	chromeOptions.put("binary", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
